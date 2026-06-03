2fca65171d87daf3c6863c08478d038718d1d7a4
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-03 17:21:51 +0200

    Skills list component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import ProfileCard from './Components/ProfileCard.jsx';
-
+import SkillsList from './Components/SkillsList.jsx';
 function App() {
     return(
         <>
             <div>
                 <h1>First react application</h1>
+
                 <ProfileCard
                     name="Jonathan" 
                     role="FullStack Software Developer"
                 />
+
+                <SkillsList />
             </div>
         </>
     );
